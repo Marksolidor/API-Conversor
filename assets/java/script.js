d470ac2274b890d26ae6fda83ede6b207f796728
Marksolidor <marco350sl@gmail.com>
2022-08-06 23:49:22 -0400

    feat calculator

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,19 +1,33 @@
 let moneySelection = document.querySelector("#selector");
 let inputCLP = document.querySelector("#clp");
 const mindiURL = "https://mindicador.cl/api/";
-
+let onDateValue = 0;
+let calculator = document.querySelector("#search");
+let dataArray = [];
+//Async Data 
 async function getMonedas() {
    try { 
     currency = moneySelection.value //Get the name in the selector for search it in the API
     const res = await fetch(`${mindiURL}${currency}`);
     const data = await res.json();
+    onDateValue = data.serie[0].valor;
     console.log(res.status);
     console.log(res)
     console.log(currency)
     console.log(data)
+    console.log(onDateValue.value)
+
    } catch (e) {
     console.error(e);
     alert("No pudimos resolver tu petición");
     return {};
 }
 };
+
+//Calculator
+calculator.addEventListener("click", (e) => {
+    let conversion = Number((onDateValue.value / onDateValue).toFixed(2));
+    document.querySelector("#result").innerHTML = conversion;
+    console.log(conversion)
+    getMonedas()
+})   
