303018d9ac7f97366298e49551521870bd2d1b01
Marksolidor <marco350sl@gmail.com>
2022-08-06 23:22:58 -0400

    feat try catch

--- assets/java/script.js
+++ assets/java/script.js
@@ -3,6 +3,7 @@ let inputCLP = document.querySelector("#clp");
 const mindiURL = "https://mindicador.cl/api/";
 
 async function getMonedas() {
+   try { 
     currency = moneySelection.value //Get the name in the selector for search it in the API
     const res = await fetch(`${mindiURL}${currency}`);
     const data = await res.json();
@@ -10,5 +11,9 @@ async function getMonedas() {
     console.log(res)
     console.log(currency)
     console.log(data)
-
+   } catch (e) {
+    console.error(e);
+    alert("No pudimos resolver tu petición");
+    return {};
 }
+};
